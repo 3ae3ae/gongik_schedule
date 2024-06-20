a98442237b0c3c53bda5e2e940e1e2bea7366646
3ae3ae <ogyshjsm@naver.com>
2024-06-20 12:24:35 +0900

    폭염 시 시간표 추가
    테마 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'dart:math';
-
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
-
 import 'package:flutter/material.dart';
-
 import 'package:url_launcher/url_launcher.dart';
 
 const schedule3 = '''|커뮤니티|대흥 순찰|새창 순찰|점심 교대
@@ -37,6 +33,37 @@ const schedule6 = '''|커뮤니티|대흥 순찰|새창 순찰|점심 교대
 13:00 ~ 15:00|D|B, C||
 15:00 ~ 17:00|E||A, F|''';
 
+const schedule3Heating = '''|커뮤니티|대흥 순찰|새창 순찰|점심 교대
+10:00 ~ 11:30|A|B, C||
+11:30 ~ 13:00||||A
+13:00 ~ 15:00|B|||
+15:00 ~ 17:00|C|||''';
+
+const schedule4Heating = '''|커뮤니티|대흥 순찰|새창 순찰|점심 교대
+09:30 ~ 10:30|C||A, B|
+10:30 ~ 11:30|A|C, D||
+11:30 ~ 13:00||||B
+13:00 ~ 15:00|C|||
+15:00 ~ 17:00|D|||''';
+
+const schedule5Heating = '''|커뮤니티|대흥 순찰|새창 순찰|점심 교대
+10:00 ~ 11:30|A|B, C|D, E|
+11:30 ~ 13:00||||A
+13:00 ~ 14:00|B|||
+14:00 ~ 15:00|C|||
+15:00 ~ 16:00|D|||
+16:00 ~ 17:00|E|||''';
+
+const schedule6Heating = '''|커뮤니티|대흥 순찰|새창 순찰|점심 교대
+10:00 ~ 11:30|A|B, C|D, E|
+11:30 ~ 13:00||||F
+13:00 ~ 14:00|A|||
+14:00 ~ 15:00||||F
+15:00 ~ 15:30|B|||
+15:30 ~ 16:00|C|||
+16:00 ~ 16:30|D|||
+16:30 ~ 17:00|E|||''';
+
 const List<String> schedules = [
   '',
   '',
@@ -45,6 +72,19 @@ const List<String> schedules = [
   schedule4,
   schedule5,
   schedule6,
+  '',
+  ''
+];
+
+const List<String> schedulesHeating = [
+  '',
+  '',
+  '',
+  schedule3Heating,
+  schedule4Heating,
+  schedule5Heating,
+  schedule6Heating,
+  '',
   ''
 ];
 
@@ -57,9 +97,9 @@ class Pair {
   }
 }
 
-String abcToName(int n, List<String> order) {
+String abcToName(int n, List<String> order, bool isHeatingMode) {
   final abc = ["A", "B", "C", "D", "E", "F"];
-  var s = schedules[n];
+  var s = isHeatingMode ? schedulesHeating[n] : schedules[n];
   for (int i = 0; i < n; ++i) {
     s = s.replaceAll(abc[i], order[i]);
   }
@@ -97,10 +137,7 @@ Future<Map<String, dynamic>> fetchNames() async {
 }
 
 void main() async {
-  runApp(MaterialApp(
-    home: const MyApp(),
-    theme: ThemeData(useMaterial3: false),
-  ));
+  runApp(const MyApp());
 }
 
 Random R = Random();
@@ -118,6 +155,7 @@ class _MyAppState extends State<MyApp> {
   List<String> members = [];
   List<bool> checkedMember = [];
   int workingMembers = 0;
+  bool isHeatingMode = false;
 
   @override
   void initState() {
@@ -154,98 +192,110 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    if (members.isEmpty) {
-      return Scaffold(
-        appBar: AppBar(title: const Text("사회복무요원 근무표")),
-        body: const Center(child: CircularProgressIndicator()),
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () async {
-            if (!await launchUrl(url)) {
-              throw Exception('Could not launch $url');
-            }
-          },
-        ),
-      );
-    } else {
-      return Scaffold(
-        appBar: AppBar(title: const Text("사회복무요원 근무표")),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () async {
-            if (!await launchUrl(url)) {
-              throw Exception('Could not launch $url');
-            }
-          },
-        ),
-        body: CustomScrollView(
-          slivers: [
-            SliverToBoxAdapter(
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Wrap(
-                  spacing: 8,
-                  runSpacing: 8,
-                  children: List.generate(
-                    members.length,
-                    (n) => Member(
-                      checkMember: checkingMember(n),
-                      checked: checkedMember[n],
-                      name: members[n],
+    const Color lightSeedColor = Colors.cyan;
+    const Color darkSeedColor = Colors.deepOrange;
+    final ThemeData lightTheme = ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: lightSeedColor, brightness: Brightness.light),
+      // useMaterial3: true,
+    );
+    final ThemeData darkTheme = ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: darkSeedColor, brightness: Brightness.dark),
+      useMaterial3: true,
+    );
+    return MaterialApp(
+      theme: isHeatingMode ? darkTheme : lightTheme,
+      home: members.isEmpty
+          ? Scaffold(
+              appBar: AppBar(
+                title: const Text("사회복무요원 근무표"),
+                actions: [
+                  Switch(
+                    value: isHeatingMode,
+                    onChanged: (value) {
+                      setState(() {
+                        isHeatingMode = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              body: const Center(child: CircularProgressIndicator()),
+              floatingActionButton: Builder(
+                builder: (context) {
+                  return FloatingActionButton(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    child: Icon(Icons.add, color: Theme.of(context).colorScheme.onPrimary),
+                    onPressed: () async {
+                      if (!await launchUrl(url)) {
+                        throw Exception('Could not launch $url');
+                      }
+                    },
+                  );
+                }
+              ),
+            )
+          : Scaffold(
+              appBar: AppBar(
+                title: const Text("사회복무요원 근무표"),
+                actions: [
+                  Switch(
+                    value: isHeatingMode,
+                    onChanged: (value) {
+                      setState(() {
+                        isHeatingMode = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              floatingActionButton: Builder(
+                builder: (context) {
+                  return FloatingActionButton(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    child: Icon(Icons.add, color: Theme.of(context).colorScheme.onPrimary),
+                    onPressed: () async {
+                      if (!await launchUrl(url)) {
+                        throw Exception('Could not launch $url');
+                      }
+                    },
+                  );
+                }
+              ),
+              body: CustomScrollView(
+                slivers: [
+                  SliverToBoxAdapter(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Wrap(
+                        spacing: 8,
+                        runSpacing: 8,
+                        children: List.generate(
+                          members.length,
+                          (n) => Member(
+                            checkMember: checkingMember(n),
+                            checked: checkedMember[n],
+                            name: members[n],
+                            isHeatingMode: isHeatingMode,
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                  SliverToBoxAdapter(
+                    child: workingMembers >= 3
+                        ? FittedBox(
+                            child: stringToDataTable(
+                                abcToName(workingMembers, mixUpOrder(), isHeatingMode)))
+                        : const SizedBox(
+                            width: 0,
+                            height: 0,
+                          ),
+                  ),
+                ],
               ),
             ),
-            SliverToBoxAdapter(
-              child: workingMembers >= 3
-                  ? FittedBox(
-                      child: stringToDataTable(
-                          abcToName(workingMembers, mixUpOrder())))
-                  : const SizedBox(
-                      width: 0,
-                      height: 0,
-                    ),
-            ),
-          ],
-        ),
-      );
-    }
+    );
   }
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: AppBar(title: const Text("사회복무요원 근무표")),
-  //     body: CustomScrollView(
-  //       slivers: [
-  //         SliverToBoxAdapter(
-  //             child: Padding(
-  //           padding: const EdgeInsets.all(10),
-  //           child: Wrap(
-  //             spacing: 8,
-  //             runSpacing: 8,
-  //             children: List.generate(
-  //               members.length,
-  //               (n) => Member(
-  //                 checkMember: checkingMember(n),
-  //                 checked: checkedMember[n],
-  //                 name: members[n],
-  //               ),
-  //             ),
-  //           ),
-  //         )),
-  //         SliverToBoxAdapter(
-  //             child: workingMembers >= 3
-  //                 ? FittedBox(
-  //                     child: stringToDataTable(
-  //                         abcToName(workingMembers, mixUpOrder())))
-  //                 : const SizedBox(
-  //                     width: 0,
-  //                     height: 0,
-  //                   )),
-  //       ],
-  //     ),
-  //   );
-  // }
 }
 
 class Member extends StatefulWidget {
@@ -253,10 +303,12 @@ class Member extends StatefulWidget {
       {super.key,
       required this.checkMember,
       required this.checked,
-      required this.name});
+      required this.name,
+      required this.isHeatingMode});
   final String name;
   final bool checked;
   final Function() checkMember;
+  final bool isHeatingMode;
 
   @override
   State<Member> createState() => _MemberState();
@@ -267,33 +319,37 @@ class _MemberState extends State<Member> {
   Widget build(BuildContext context) {
     final checked = widget.checked;
     final name = widget.name;
-    Color newColor = checked ? Colors.red : Colors.blue;
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    final Color backgroundColor = checked ? colorScheme.tertiary : colorScheme.primary;
+    final Color textColor = checked ? colorScheme.onTertiary : colorScheme.onPrimary;
+    
     return TweenAnimationBuilder<Color?>(
-        tween: ColorTween(begin: Colors.blue, end: newColor),
-        duration: const Duration(milliseconds: 500),
-        builder: (_, Color? color, __) {
-          return ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: color,
-            ),
-            onPressed: () {
-              widget.checkMember();
-            },
-            child: checked
-                ? FittedBox(
-                    child: Row(
-                      children: [
-                        const Icon(Icons.check),
-                        const SizedBox(
-                          width: 5,
-                          height: 5,
-                        ),
-                        Text(name)
-                      ],
-                    ),
-                  )
-                : Text(name),
-          );
-        });
+      tween: ColorTween(begin: colorScheme.primary, end: backgroundColor),
+      duration: const Duration(milliseconds: 500),
+      builder: (_, Color? color, __) {
+        return ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: color,
+          ),
+          onPressed: () {
+            widget.checkMember();
+          },
+          child: checked
+              ? FittedBox(
+                  child: Row(
+                    children: [
+                      Icon(Icons.check, color: textColor),
+                      const SizedBox(
+                        width: 5,
+                        height: 5,
+                      ),
+                      Text(name, style: TextStyle(color: textColor))
+                    ],
+                  ),
+                )
+              : Text(name, style: TextStyle(color: textColor)),
+        );
+      },
+    );
   }
 }
